d360923c780a84e857293bf5804ba229bffe36a6
martincarmen03 <martincarmen03@gmail.com>
2016-07-16 01:05:34 -0700

    created variable and function for portland airport and called fuction

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Pioneer Square
 var pioneerSquare = {
     locationName: "Pioneer Square",
     minCust: 17,
@@ -14,11 +15,11 @@ randomCustomersPerHour: function() {
             listBuilt += '<tr><td>';
         var randomCookiesSales = Math.random() * (this.maxCust - this.minCust);
         this.salesHour.push(parseInt(randomCookiesSales));
-        var hour = this.openHour;
-        if (hour < 12) {
+        var time = this.openHour;
+        if (time < 12) {
             listBuilt += this.openHour;
             listBuilt += ' am: ';
-        } else if (hour == 12) {
+        } else if (time == 12) {
             listBuilt += this.openHour;
             listBuilt += ' pm: ';
         } else {
@@ -40,10 +41,52 @@ randomCustomersPerHour: function() {
       console.log("Built Pioneer Square List");
     }
 }
+//Portland Airport
+var portlandAirport = {
+    locationName: "Portland Airport",
+    minCust: 17,
+    maxCust: 88,
+    averageSales: 5.2,
+    openHour: 10,
+    closeHour: 18,
+    salesHour: [],
 
+randomCustomersPerHour: function() {
+      var listBuilt = '';
+          listBuilt += '<th>' + this.locationName +'</th>';
+      for (var index = this.openHour; index <= this.closeHour; index++) {
+            listBuilt += '<tr><td>';
+        var randomCookiesSales = Math.random() * (this.maxCust - this.minCust);
+        this.salesHour.push(parseInt(randomCookiesSales));
+        var time = this.openHour;
+        if (time < 12) {
+            listBuilt += this.openHour;
+            listBuilt += ' am: ';
+        } else if (time == 12) {
+            listBuilt += this.openHour;
+            listBuilt += ' pm: ';
+        } else {
+            listBuilt += (this.openHour - 12);
+            listBuilt += ' pm: ';
+        }
 
+            listBuilt += '</td>';
+            listBuilt += '<td>';
+            listBuilt += " ";
+            listBuilt += this.salesHour[index - 10];
+            listBuilt += '</td>';
+            listBuilt += '</tr>';
+            this.openHour++;
+        }
+
+      var listsElem = document.getElementById("portlandAirport");
+      listsElem.innerHTML = listBuilt;
+      console.log("Built Portland Airport List");
+    }
+}
 
 
 
 
 pioneerSquare.randomCustomersPerHour();
+portlandAirport.randomCustomersPerHour();
